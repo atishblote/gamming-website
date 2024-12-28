60f5293a30d65079482a54427593765462dc9a22
btmdevatish <atish@buzztagmedia.com>
2024-12-28 18:22:57 +0530

    Add Api data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  allData:any
+  constructor(private global: GlobalService){}
+
+  ngOnInit(): void {
+    this.global.getWithToken('all-listings').subscribe({
+      next:(res:any)=>{
+        this.allData = res.data
+        console.log(this.allData)
+      },
+      error: (err:any)=>{
+        console.log(err.error)
+      }
+    })
+  }
 
 }
